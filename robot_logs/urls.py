9e01d089b7976705d09d48851635297acc658440
FyFou <40236744+FyFou@users.noreply.github.com>
2025-03-15 22:35:37 +0100

    Mise à jour des URLs pour inclure les nouvelles vues CAN et DBC

--- robot_logs/urls.py
+++ robot_logs/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from . import views_dbc
+from . import views_can
 
 app_name = 'robot_logs'
 
@@ -18,4 +20,16 @@ urlpatterns = [
     path('log/<int:log_id>/curve/', views.CurveDataView.as_view(), name='curve_view'),
     path('log/<int:log_id>/laser/', views.Laser2DView.as_view(), name='laser_view'),
     path('log/<int:log_id>/image/', views.ImageDataView.as_view(), name='image_view'),
+    
+    # Nouvelles vues pour les fichiers DBC
+    path('dbc-files/', views_dbc.DBCFileListView.as_view(), name='dbc_file_list'),
+    path('dbc-files/upload/', views_dbc.DBCFileUploadView.as_view(), name='dbc_file_upload'),
+    path('dbc-files/<int:pk>/', views_dbc.DBCFileDetailView.as_view(), name='dbc_file_detail'),
+    path('dbc-files/<int:pk>/delete/', views_dbc.DBCFileDeleteView.as_view(), name='dbc_file_delete'),
+    
+    # Nouvelles vues pour les données CAN
+    path('log/<int:log_id>/can/', views_can.CANDataView.as_view(), name='can_view'),
+    path('log/<int:log_id>/can/export/', views_can.CANExportView.as_view(), name='can_export'),
+    path('can-message/<int:message_id>/', views_can.CANMessageDetailView.as_view(), name='can_message_detail'),
+    path('log/<int:log_id>/can/filter/<str:can_id>/', views_can.CANIDFilterView.as_view(), name='can_id_filter'),
 ]
