e2bb7e3da6fd0847d0514a24b285219d1125857e
FyFou <40236744+FyFou@users.noreply.github.com>
2025-03-15 16:56:40 +0100

    Ajout de la configuration de l'application

---
+++ robot_logs/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RobotLogsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'robot_logs'
+    verbose_name = 'Logs de Robots'
